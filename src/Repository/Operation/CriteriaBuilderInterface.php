78ae1b600f4a75930f2623caa4c60ada705c8a52
KUBO Atsuhiro <kubo@iteman.jp>
2014-10-23 12:06:27 +0900

    [Repository] add CriteriaBuilderInterface #3

---
+++ src/Repository/Operation/CriteriaBuilderInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright (c) 2014 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Domain Kata.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\DomainKata\Repository\Operation;
+
+use PHPMentors\DomainKata\Entity\CriteriaInterface;
+
+interface CriteriaBuilderInterface extends CriteriaInterface, OperationInterface
+{
+    /**
+     * @return \PHPMentors\DomainKata\Entity\CriteriaInterface
+     */
+    public function build();
+}
